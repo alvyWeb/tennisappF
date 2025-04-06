d35052a1a1772bca14849e28597b00d0ad0dc507
alvyWeb <alvy@npcoding.com>
2025-04-07 03:41:55 +0600

    Update route.js

--- app/api/verify/route.js
+++ app/api/verify/route.js
@@ -12,7 +12,7 @@ import { cert, getApps, initializeApp } from "firebase-admin/app";
 if (!getApps().length) {
   initializeApp({
     credential: cert({
-      projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+      projectId: 'atpenn-4fc94',
       clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
       privateKey: process.env.FIREBASE_PRIVATE_KEY,
     }),
