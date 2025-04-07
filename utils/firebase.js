0ef45243033f3678ec11b2ac5d0a63ca22ca571a
alvyWeb <alvy@npcoding.com>
2025-04-07 22:21:55 +0600

    Update

--- utils/firebase.js
+++ utils/firebase.js
@@ -2,15 +2,13 @@ import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
-console.log("API KEY:", process.env.NEXT_PUBLIC_FIREBASE_API_KEY);
-
 const firebaseConfig = {
-    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+    apiKey: 'AIzaSyCg8k73ur8zsih0dNyBJjPNOsSc3uYKm28',
+    authDomain: 'atpenn-4fc94.firebaseapp.com',
+    projectId: 'atpenn-4fc94',
+    storageBucket: 'atpenn-4fc94.appspot.com',
+    messagingSenderId: '851879336191',
+    appId: '1:851879336191:web:0a61867a3ab3ffc02554ca',
 };
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
